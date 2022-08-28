396f0df71ff0d20e45058b7ed14475adef07001b
Chico <fjlainfo@gmail.com>
2022-08-28 11:59:58 -0300

    findone test

--- test/pokemon.test.js
+++ test/pokemon.test.js
@@ -11,7 +11,13 @@ jest.mock('../src/config/mongo', () => {
                 async find() {
                     return [{ name: 'mock'}]
                 },
+                async findOne() {
+                    return { _id: '630a7d042c2ad025fcbacb4b', name: 'mock'}
+                },
                 async insert() {
+                    return {_id: '000999'}
+                },
+                async update() {
                     return {_id: '000999'}
                 }
             }
@@ -33,8 +39,13 @@ it('GET /pokemons', async() =>{
 it('GET /pokemons/:id', async() =>{
     const response = await app.inject({
         method: 'GET',
-        url: '/pokemons'
+        url: '/pokemons/630a7d042c2ad025fcbacb4b'
     })
+    const body = JSON.parse(response.body)
+    expect(response.statusCode).toBe(200)
+    expect(body.name).toBe('mock')
+    //comparar objeto
+    expect(body).toEqual({_id: '630a7d042c2ad025fcbacb4b', name:'mock'})
 })
 
 it('POST /pokemons', async() =>{
@@ -49,11 +60,16 @@ it('POST /pokemons', async() =>{
     expect(body._id).toBeDefined()
 })
 
-it('PUT /pokemons', () =>{
-    app.inject({
-        method: 'GET',
-        url: '/pokemons'
+it('PUT /pokemons', async() =>{
+    const response = await app.inject({
+        method: 'PUT',
+        url: '/pokemons',
+        headers: {'content-type': 'application/json'},
+        body: JSON.stringify({ name: 'NomeTeste'})
     })
+    //const body = JSON.parse(response.body)
+    //expect(response.statusCode).toBe(201)
+    //expect(body._id).toBeDefined()
 })
 
 it('DELETE /pokemons', () =>{
