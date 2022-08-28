48c276610febec429b7b7c58fdb47141bb54f9cd
Chico <fjlainfo@gmail.com>
2022-08-28 14:27:31 -0300

    APi harry Potter

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const { rmSync } = require('fs')
 const { err } = require('pino-std-serializers')
 const app = fastify()
 const controller = require('./controller/pokemon')
+const hpController = require('./controller/hp')
 
 app.get('/', (req, res) => res.send('pong'))
 app.get('/pokemons', controller.list)
@@ -11,6 +12,8 @@ app.get('/pokemons/:id', controller.byId)
 app.post('/pokemons', controller.create)
 app.put('/pokemons/:id', controller.update)
 app.delete('/pokemons/:id', controller.delete)
+
+app.get('/harry-potter', hpController.list)
 
 app.setNotFoundHandler((req, res) =>{
     res.status(404).send('Not Found')
---
+++ src/controller/hp.js
@@ -0,0 +1,19 @@
+const ENDPOINT = 'http://hp-api.herokuapp.com/api/characters'
+const axios = require('axios')
+
+const controller = {
+    list(req, res) {
+        return axios.get(ENDPOINT)
+            .then(result => {
+                const data = result.data.map(item =>{
+                    return {
+                        name: item.name,
+                        gender: item.gender
+                    }
+                })
+                res.send(data)
+        })
+    }
+}
+
+module.exports = controller
